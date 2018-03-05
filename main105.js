3a5a9fea294f6398ba2c340b47e69e30a4b729be
semeklis <37065477+semeklis@users.noreply.github.com>
2018-03-05 22:09:47 +0200

    Update main105.js

--- main105.js
+++ main105.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-    $.ajax({url: "https://api.kraken.com/0/public/Ticker?pair=XXBTZEUR", success: function(result){
-        debugger;
-        result = result.result;
-        var data = result.XXBTZEUR;
-        var cValues = data.c;
-        var requiredVal = cValues[0].toFixed(2);
-        requiredVal = parseFloat(requiredVal);
 
-        var multiplier = 1.05;
+    apiCall();
+    function apiCall() {
+        $.ajax({url: "https://api.kraken.com/0/public/Ticker?pair=XXBTZEUR", success: function(result){
+            result = result.result;
+            var data = result.XXBTZEUR;
+            var cValues = data.c;
+            var requiredVal = cValues[0];
+            requiredVal = parseFloat(requiredVal);
 
-        $("#data").text(requiredVal * multiplier);
-    }});
+            var multiplier = 1.05;
+            var answer = requiredVal * multiplier;
+            $("#data").text(answer.toFixed(2));
+        }});
+    }
+    window.setInterval(function(){
+        apiCall();
+    }, 2000);
+
 });
